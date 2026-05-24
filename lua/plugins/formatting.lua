4e9bd07f067fc980297897b8fe35fca3c4698716
Soichiro Jo <jo-soichiro334@g.ecc.u-tokyo.ac.jp>
2026-05-24 21:57:25 +0900

    fix: python formatter
    change python formatter from black to ruff

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,36 +1,40 @@
 -- ~/.config/nvim/lua/plugins/formatting.lua
 
 return {
-  {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "isort", "black" },
-        javascript = { "prettierd", "prettier", stop_after_first = true },
-        typescript = { "prettierd", "prettier", stop_after_first = true },
-        json = { "prettierd", "prettier", stop_after_first = true },
-        html = { "prettierd", "prettier", stop_after_first = true },
-        css = { "prettierd", "prettier", stop_after_first = true },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
+    {
+        "stevearc/conform.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        opts = {
+            formatters_by_ft = {
+                lua = { "stylua" },
+                python = {
+                    "ruff_fix",
+                    "ruff_format",
+                    "ruff_organize_imports",
+                },
+                javascript = { "prettierd", "prettier", stop_after_first = true },
+                typescript = { "prettierd", "prettier", stop_after_first = true },
+                json = { "prettierd", "prettier", stop_after_first = true },
+                html = { "prettierd", "prettier", stop_after_first = true },
+                css = { "prettierd", "prettier", stop_after_first = true },
+            },
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
+        },
+        keys = {
+            {
+                "<leader>f",
+                function()
+                    require("conform").format({
+                        async = true,
+                        lsp_format = "fallback",
+                    })
+                end,
+                mode = "",
+                desc = "Format buffer",
+            },
+        },
     },
-    keys = {
-      {
-        "<leader>f",
-        function()
-          require("conform").format({
-            async = true,
-            lsp_format = "fallback",
-          })
-        end,
-        mode = "",
-        desc = "Format buffer",
-      },
-    },
-  },
 }
